6c3fa67257d12c4c6586ca4fca8a03f59716027f
Andrew Plaza <andrew.plaza15@gmail.com>
2016-12-29 00:44:50 -0500

    renamed webpack to react-dev, something i enjoy more

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   //devtool: 'cheap-module-source-map',
   devtool: 'eval',
   // webpack folder's entry js - excluded from jekll's build process.
-  entry: "./webpack/entry.js",
+  entry: "./react-dev/entry.js",
   output: {
     // we're going to put the generated file in the assets folder so jekyll will grab it.
       path: 'src/_assets/js/',
