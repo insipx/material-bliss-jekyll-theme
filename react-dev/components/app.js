d98c02541733321c823656ead909945c77d84a78
Andrew Plaza <andrew.plaza15@gmail.com>
2016-12-31 02:52:02 -0500

    added app component, routes, and a css library

---
+++ react-dev/components/app.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+//MUI React Library
+import { cyan500 } from 'material-ui/styles/colors';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import Menu from '../components/Menu';
+
+const muiTheme = getMuiTheme(darkBaseTheme, {
+  appBar: {
+    height: 50,
+  },
+  });
+
+export default class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+          <Menu />
+      </MuiThemeProvider>
+    );
+  }
+}
---
+++ react-dev/routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Route } from 'react-router';
+import App from './components/app';
+
+export default (
+  <Route path="/" component={App} />
+);
