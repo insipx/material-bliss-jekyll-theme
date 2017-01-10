7c1894dfb8b2331a42f23b5bad394dc344187697
Andrew Plaza <andrew.plaza15@gmail.com>
2017-01-10 03:15:43 -0500

    improved social media component

--- react-dev/components/social_media_list.js
+++ react-dev/components/social_media_list.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FontIcon from 'material-ui/FontIcon';
 import { green900 } from 'material-ui/styles/colors';
+import _ from 'lodash';
 
 const iconStyles = {
   icon: {
@@ -8,18 +9,51 @@ const iconStyles = {
   }
 };
 
-const icons = [
-  ' zmdi-github',
-  'zmdi-facebook',
-  'zmdi-twitter',
-  'zmdi-google-plus',
-  'zmdi-instagram',
-  'zmdi-rss'
-];
+//merge into one object and do it that way
+const icons = {
+  github: 'zmdi-github',
+  facebook: 'zmdi-facebook',
+  twitter: 'zmdi-twitter',
+  gplus: 'zmdi-google-plus',
+  coding: 'zmdi-coding',
+  rss: 'zmdi-rss'
+};
 
-const renderIcons = () => icons.map(icon => (
-          <FontIcon className={`zmdi ${icon}`} style={iconStyles.icon} />
-  ));
+const urls = {
+  github: 'http://www.github.com/',
+  facebook: 'http://www.facebook.com/',
+  twitter: 'http://www.twitter.com/',
+  gplus: 'https://plus.google.com/',
+  coding: 'http://www.livecoding.tv/',
+  rss: 'http://www.test_domain.com:4000/'
+};
+
+//iterate over urls and create the right url based on social media link,
+//then render the FontIcon
+const renderIcons = () => {
+    const result = [];
+  _.forEach(urls, (value, key) => {
+    result.push(
+      <FontIcon
+        className={`zmdi ${icons[`${key}`]}`}
+        style={iconStyles.icon}
+        hoverColor={green900}
+        key={key}
+      />);
+});
+    return result.map(element => element);
+};
+
+/*
+
+          <a href={`${key}${icons.value}`} key={icons.value} >
+            <FontIcon
+              className={`zmdi ${icons.value}`}
+              style={iconStyles.icon}
+              hoverColor={green900}
+            />
+          </a>
+ */
 
 export const SocialMediaList = (props) => (
     <span style={props.style}>
