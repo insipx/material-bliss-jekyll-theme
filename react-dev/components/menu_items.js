3b8735b2ea542c48440ed38f521958d621fe8ebd
Andrew Plaza <andrew.plaza15@gmail.com>
2017-01-13 15:39:12 -0500

    more responsiveness and modularizing header a bit more to allow for dynamicm resizing of menu

---
+++ react-dev/components/menu_items.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import _ from 'lodash';
+
+import MenuItem from 'material-ui/MenuItem';
+import { Card, CardActions, CardHeader, CardTitle, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import { SocialMediaList } from './social_media_list';
+
+import { getLink } from '../helpers';
+
+const menuItems = { Home: '/', About: '/about/', Projects: '/projects/' };
+
+const styles = {
+  spanSocial: {
+    float: ' left',
+    paddingLeft: '25%',
+    paddingTop: 20,
+  }
+};
+const getMenuItem = (name, path, url) => {
+return getLink(
+    <MenuItem>{name}</MenuItem>,
+    name,
+    url, //the site root url
+    path
+  );
+};
+
+const renderMenuItems = (url) => {
+  const result = [];
+   _.forEach(menuItems, (value, key) => {
+      result.push(getMenuItem(key, value, url));
+  });
+  return result.map((item) => { return item; });
+};
+
+export const MenuItems = props => {
+return (
+    <div>
+      {renderMenuItems(props.config.url)}
+      <Card>
+        <CardHeader
+          title={props.config.name}
+          subtitle={props.config.menu_right_subtitle}
+          avatar={props.config.avatar}
+        />
+        <CardTitle title="About" />
+        <CardText>
+          {props.config.description}
+        </CardText>
+        <CardActions>
+          {getLink(
+            <RaisedButton label="More About Me" primary />,
+            '',
+            props.config.url,
+            '/about/'
+          )}
+        </CardActions>
+      </Card>
+      <SocialMediaList style={styles.spanSocial} social={props.config.social} />
+  </div>
+  );
+};
