8bf5892e452f9cd1759ca7aefc912c106c0c9257
Andrew Plaza <andrew.plaza15@gmail.com>
2017-01-13 13:14:12 -0500

    got masonry layout working

--- react-dev/components/projects_index.js
+++ react-dev/components/projects_index.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Masonry from 'react-masonry-component';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
 
 import { fetchProjects, fetchSiteInfo } from '../actions/index';
@@ -14,18 +15,37 @@ class ProjectsIndex extends Component {
   componentWillMount() {
     this.props.fetchProjects();
   }
+  getMedia = (project) => {
+      if (project.image) {
+        return (
+          <CardMedia
+            overlay={<CardTitle title="Cool Project" subtitle="hellya" />}
+          >
+            <img role="presentation" src={`${this.props.config.url}/${project.image}`} />
+        </CardMedia >
+        );
+      }
+      return;
+  }
+  getAuthorHeader = (project) => {
+      if (project.author && project.author_tag && project.avatar) {
+        return (
+          <CardHeader title={project.author} subtitle={project.author_tag} avatar={this.props.config.avatar} />
+        );
+      }
+      return;
+  }
 
   renderProjects() {
     return this.props.projects[1].map((project) => (
-        <Col xs={3} key={project.title}>
+        <div className="masonry-content" key={project.title}>
           <Card>
-            <CardMedia overlay={<CardTitle title="Cool Project" subtitle="yep. that cool." />} >
-               <img role="presentation" src={`${this.props.config.url}/${project.image}`} />
-            </CardMedia>
+              {this.getAuthorHeader(project)}
+              {this.getMedia(project)}
             <CardTitle title={project.title} subtitle={project.subtitle} />
             <CardText> {project.description} </CardText>
           </Card>
-        </Col>
+        </div>
       ));
   }
 
@@ -37,40 +57,10 @@ class ProjectsIndex extends Component {
     }
     return (
       <Grid>
-        <Row start="xs">
-          <Col xs={3} >
-            <Card>
-              <CardHeader title="Andrew Plaza" subtitle="You can also format it like this!" avatar={this.props.config.avatar} />
-              <CardTitle title="Card Title" subtitle="Card subtitle" />
-              <CardText>
-                Project Excerpt....Sweet!! Something about my project that is really cool! Like, this project basically bootstraps a rocket ship and flies you to the moon, for free! It's the next great thing, next to...er, oreos. Oreos are pretty great.
-              </CardText>
-            </Card>
-          </Col>
+        <Masonry>
           {this.renderProjects()}
-          <Col xs={3}>
-            <Card>
-              <CardHeader
-                title="Linus Torvalds"
-                subtitle="Or even like this "
-                avatar={this.props.config.avatar}
-              />
-              <CardMedia
-                overlay={<CardTitle title="Cool Project" subtitle="yep. that cool." />}
-              >
-              <img
-                role="presentation"
-                src={`${this.props.config.url}/static/img/placeholder.jpg`}
-              />
-             </CardMedia>
-              <CardTitle title="Card Title" subtitle="Card subtitle" />
-              <CardText>
-                Hey! YOU. Did you know that this uses a FlexBox Layout? You can arrange these cards anyway you want! Right up to your hearts content.
-              </CardText>
-            </Card>
-          </Col>
-        </Row>
-      </Grid>
+      </Masonry>
+    </Grid>
     );
   }
 }
