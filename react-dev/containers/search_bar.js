f729e2209fe77426adcd66403d1ed21a08ff3149
Andrew Plaza <andrew.plaza15@gmail.com>
2017-01-16 20:26:29 -0500

    added responsive search bar

--- react-dev/containers/search_bar.js
+++ react-dev/containers/search_bar.js
@@ -1,16 +1,70 @@
 import React, { Component } from 'react';
-
+import _ from 'lodash';
 import { connect } from 'react-redux';
 
 import IconButton from 'material-ui/IconButton';
+import AutoComplete from 'material-ui/AutoComplete';
 
 import { fetchPosts } from '../actions';
 
 class SearchBar extends Component {
 
+  getSearchBarWidth() {
+    const width = this.props.getMenuWidth();
+    if (width === 400) return 200;
+    else if (width === 350) return 175;
+    else if (width === 300) return 150;
+    else if (width === 256) return 125;
+  }
+
+  styles = {
+    root: {
+      width: this.getSearchBarWidth(),
+    },
+    textField: {
+      width: this.getSearchBarWidth(),
+    }
+  }
+
+  handleClick() {
+    console.log('click');
+    const toggleBox = document.getElementById('search-form-auto');
+    if (!toggleBox) {
+      return;
+    }
+    console.log('its not null!');
+    toggleBox.classList.toggle('search-hidden');
+  }
+
+  postTitles = () => this.props.posts.map(post => post.title)
+
   render() {
+    console.log(this.props.posts);
+    if (_.isEmpty(this.props.posts)) {
+      return (
+        <div />
+      );
+    }
     return (
-      <IconButton iconClassName="zmdi zmdi-search" tooltip="Search" tooltipPosition="top-center" />
+      <div id="container">
+          <div id='search-form-auto' className="search-hidden" style={{ float: 'left', }}>
+            <AutoComplete
+              hintText="Enter Search..."
+              filter={AutoComplete.fuzzyFilter}
+              dataSource={this.postTitles()}
+              maxSearchResults={5}
+              style={this.styles.root}
+              textFieldStyle={this.styles.textField}
+            />
+          </div>
+          <IconButton
+            iconClassName="zmdi zmdi-search"
+            tooltip="Search"
+            tooltipPosition="bottom-center"
+            touch
+            onTouchTap={this.handleClick}
+          />
+      </div>
     );
   }
 }
